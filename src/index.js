ec5097ee588d004d291bd21bb6b522349c630405
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2024-08-27 17:40:39 -0400

    completed first draft

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
-import "./styles.css"
-import { greeting } from "./test.js";
+import "./styles.css";
 
-console.log(greeting);
+async function getWeatherData(location) {
+  const query = location;
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=RPQEBHWSNE2LNVFN6W8EK4HMT`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+    return processWeatherData(data);
+  } catch (error) {
+    console.error("Error fetching the weather data:", error);
+    alert("Invalid entry.");
+    return null;
+  }
+}
+
+function processWeatherData(data) {
+  return {
+    location: data.address,
+    temperature: data.currentConditions.temp,
+    feelsLike: data.currentConditions.feelslike,
+    conditions: data.currentConditions.conditions,
+  };
+}
+
+const locationInput = document.getElementById("location-input");
+const locationSearch = document.getElementById("location-search");
+
+locationSearch.addEventListener("click", async () => {
+  const weatherData = await getWeatherData(locationInput.value);
+  if (weatherData) {
+    renderWeatherCard(weatherData);
+  }
+});
+
+function renderWeatherCard(wdata) {
+  const content = document.getElementById("content");
+
+  if (content) {
+    content.innerHTML = `
+            <div class="weather-card">
+            <h1>${wdata.location.toUpperCase()}</h1>
+            <p>Temperature: ${wdata.temperature}°C</p>
+            <p>Feels Like: ${wdata.feelsLike}°C</p>
+            <p>Conditions: ${wdata.conditions}</p>
+            </div>
+        `;
+  }
+}
